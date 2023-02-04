9570ece628f038883ecaee15e28d9b71b664185b
rai <sarthakrai3406@gmail.com>
2023-02-04 19:17:41 +0530

    refresh, yay

--- index.js
+++ index.js
@@ -1,181 +1,168 @@
-window.jsPDF = window.jspdf.jsPDF
-
-//Generate Item Cards
 fetch('items.json')
-.then(response=>response.json())
-.then(data=>{
-    //Extract JSON objects
-    const items=data.items;
-    //HTML template
-    const catalogueTemplate=items.map(item=>{
-        let selected = "";
-        if(item.status === 'in-stock'){
-            selected = " selected";
-        }
-        return `
-        <div class="item selectable">
-            <img src="images/${item.image}" alt="${item.description}">
-            <h2>${item.name}</h2>
-            <p>Price: ${item.price}¥</p>
-            <div class="content">
-                <p class="description">${item.description}</p>
-                <p class="status">${item.status == "in stock" ? "In Stock" : "Out of Stock"}</p>
-            </div>
-        </div>
-        `;
-    }).join("");
-    //Insert into page
-    const catalogue = document.querySelector(".catalogue");
-    catalogue.innerHTML=catalogueTemplate;
-});
+  .then(response => response.json())
+  .then(data => {
+    const items = data.items
+    const categories = [
+      "Seating",
+      "Sleeping or Lying",
+      "Entertainment",
+      "Tables",
+      "Storage",
+      "Bedroom",
+      "Living",
+      "Dining",
+      "Vanity",
+      "Patio"
+    ];
 
-//Submit button state
-const orderButton = document.getElementById("order-button");
-function checkform() {
-    const formElements = document.forms["order-form"].elements;
-    let submitBtnActive = true;
-    for (let inputEl = 0; inputEl < formElements.length; inputEl++) {
-        if (formElements[inputEl].value.length == 0) submitBtnActive = false;
-    }
-      if (submitBtnActive) {
-        orderButton.disabled = false;
-    } else {
-        orderButton.disabled = "disabled";
-    }
-}
-  
+    // Featured items on home page
 
-//Make card clickable, and add to form
-const formItems = document.querySelector('#itemsf');
-const catalogue = document.querySelector(".catalogue");
-catalogue.addEventListener('click', function(e) {
-    if(e.target.classList.contains('item')) {
-        if (e.target.classList.contains('selected')) {
-            e.target.classList.remove('selected');
-        } else {
-            e.target.classList.add('selected');
-        }
-        updateItemsField();
-    }
-});
-function updateItemsField(){
-    const selectedItems = document.querySelectorAll('.selected h2');
-    let itemsValue = "";
-    selectedItems.forEach(item => {
-        itemsValue += item.innerText + ', ';
+    let categoryItem;
+
+    const display = categories.map(category => {
+      categoryItem = items.find(item => item.category === category);
+
+      if (!categoryItem) {
+        return `<div class="card seccard">
+                  <h2>${category}</h2>
+                  <p>Work In Progress</p>
+                </div>`;
+      }
+
+      return `<div class="card">
+                <h2>${category}</h2>
+                <img src="${categoryItem.image}" alt="${categoryItem.name}" />
+              </div>`;
     });
-    formItems.value = itemsValue.slice(0,-2);
-}
 
-//Process order
-orderButton.addEventListener("click", function(event) {
-    event.preventDefault();
-    const haveItBuilt = document.getElementById("select-button").value;
-    let buttonsHTML = "";
-    if(haveItBuilt === "yes"){
-        buttonsHTML = `
-            <p>Your order will be processed shortly. Thank you for shopping with us!</p>
-            <button id="download-invoice" onclick="generateInvoice()">Download Invoice</button>
-            <button id="close-popup" onclick="document.querySelector('.popup').style.display = 'none'; document.getElementById('overlay').style.display = 'none'; return false;">Close</button>
-        `;
-    }else{
-        buttonsHTML = `
-            <p>Your order will be processed shortly. Thank you for shopping with us!</p>
-            <button id="download-instructions">Download Instructions</button>
-            <button id="download-invoice" onclick="generateInvoice()">Download Invoice</button>
-            <button id="close-popup" onclick="document.querySelector('.popup').style.display = 'none'; document.getElementById('overlay').style.display = 'none'; return false;">Close</button>
-        `;
-    }
-    const popup = document.createElement("div");
-    popup.innerHTML = buttonsHTML;
-    popup.classList.add("popup");
-    document.body.appendChild(popup);
-    const overlay = document.getElementById("overlay");
-    overlay.style.display = "block";
+    document.querySelector(".featured-items").innerHTML = display.join("");
 
-    popup.addEventListener("click", function(event) {
-        if (event.target.id === "close-popup") {
-            document.getElementById("name").value = "";
-            document.getElementById("discord-username").value = "";
-            document.getElementById("address").value = "";
-            document.getElementById("itemsf").value = "";
-            document.getElementById("select-button").selectedIndex = 0;
+    // Search items
 
-            const selectedItems = document.querySelectorAll('.selected');
-            for(let i=0; i<selectedItems.length; i++) {
-                selectedItems[i].classList.remove('selected');
-            }
+    const searchBtn = document.querySelector(".search-icon");
+    const backBtn = document.querySelector(".back-button");
+    const searchInput = document.querySelector(".search-box");
+    const featuredItemsContainer = document.querySelector(".featured-items-container");
+    const searchResult = document.querySelector(".search-result");
+    const searchResultContainer = document.querySelector(".search-result-container");
 
+    let cart = [];
 
-            this.remove();
-        }
-    });
+    searchBtn.addEventListener("click", function() {
+        const searchTerm = searchInput.value;
+        // Hide the featured items container
+        featuredItemsContainer.style.display = "none";
     
-});
+        // Clear any previous search results
+        searchResult.innerHTML = "";
+    
+        const filteredItems = items.filter(item =>
+          item.name.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+    
+        // Show the search result section
+        searchResult.style.display = "flex";
+    
+        // Add the filtered items as cards to the search result section
+        filteredItems.forEach(item => {
+            const card = document.createElement("div");
+            card.classList.add("card");
+            card.innerHTML = `
+            <h2>${item.name}</h2>
+            <img src="${item.image}" alt="${item.name}" />
+            <p>${item.price} ¥</p>
+            <p>${item.description}</p>
+            <button class="add-to-cart">Add to Cart</button>
+            `;
+            searchResult.appendChild(card);
 
-//Function to generate the invoice
-function generateInvoice() {
-    //Get the form data
-    const name = document.getElementById("name").value;
-    const address = document.getElementById("address").value;
-    const itemsInput = document.getElementById("itemsf").value;
-    let selectedItems = itemsInput.split(", ");
-    //Get the items data
-    fetch('items.json')
-    .then(response=>response.json())
-    .then(data=>{
-        const items = data.items;
-        //Initialize the PDF document
-        const pdf = new jsPDF("p", "mm", "a4");
-
-        //Add the company information
-        pdf.setFontSize(14);
-        pdf.text("Aikia", 14, 22);
-        pdf.text("Matsuri-Machi Mall, Enganchika", 14, 32);
-
-        //Add the invoice title
-        pdf.setFontSize(18);
-        pdf.text("Invoice", 14, 50);
-
-        //Add the customer information
-        pdf.setFontSize(14);
-        pdf.text("Name: " + name, 14, 70);
-        pdf.text("Address: " + address, 14, 80);
-
-        //Add the invoice items table header
-        pdf.setFontSize(12);
-        pdf.text("Description", 14, 100);
-        pdf.text("Price", 100, 100);
-
-        //Add the items and prices
-        var y = 110;
-        items.forEach(function(item) {
-            if(selectedItems.indexOf(item.name) !== -1) {
-                pdf.setFontSize(12);
-                pdf.text(item.name, 14, y);
-                pdf.text(item.price + "¥", 100, y);
-                y += 10;
-            }
+            card.querySelector(".add-to-cart").addEventListener("click", function() {
+                cart.push(item.name);
+                console.log(cart);
+                if (localStorage.getItem("cart")===null) {
+                    localStorage.setItem("cart", JSON.stringify(cart))
+                } else {
+                    const storedCart = JSON.parse(localStorage.getItem("cart"));
+                    cart.forEach(item => {
+                        // Check if the item is already in the storedCart
+                        const index = storedCart.indexOf(item);
+                        if (index === -1) {
+                        // If not, add it to the storedCart
+                        storedCart.push(item);
+                        }
+                    });
+                    // Update localStorage with the new cart
+                    localStorage.setItem("cart", JSON.stringify(storedCart));
+                }
+            });
         });
-
-        //Add the dividing line
-        pdf.setLineWidth(1);
-        pdf.line(10, y, 200, y);
-        y += 10;
-
-        //Add the total cost
-        pdf.setFontSize(14);
-        pdf.text("Amount to be paid: ", 14, y);
-        var totalCost = 0.0;
-        items.forEach(function(item) {
-            if(selectedItems.indexOf(item.name) !== -1) {
-                totalCost += parseFloat(item.price);
-            }
-        });
-        pdf.text(totalCost.toFixed(2) + "¥", 100, y);
-        y += 10;
-
-        //Save the PDF
-        pdf.save("invoice.pdf");
     });
-}
+
+    backBtn.addEventListener("click", function() {
+        searchResultContainer.style.display = "none";
+        featuredItemsContainer.style.display = "flex";
+        window.location.reload(true);
+    });
+
+    // Section pages
+    const cards = document.querySelectorAll('.seccard');
+    const navLinks = document.querySelectorAll('.navigation a');
+    const sections = document.querySelectorAll('section');
+
+    const createBodyContainer = function(headerText) {
+        // Create the section element
+        const bodyContainer = document.createElement('section');
+        bodyContainer.classList.add('gen-sec','body-container');
+
+        // Create the div for the header
+        const bodyHeadContainer = document.createElement('div');
+        bodyHeadContainer.classList.add('body-head-container');
+
+        // Create the h1 element and set its value
+        const bodyHead = document.createElement('h1');
+        bodyHead.innerText = headerText;
+
+        const bodyHeadContainerT = document.createElement('div');
+        bodyHeadContainerT.classList.add('body-head-container');
+
+        const backButton = document.createElement('i');
+        backButton.classList.add('fa-solid', 'fa-backward', 'back-button');
+        bodyHeadContainerT.appendChild(backButton);
+
+        // Append the h1 to the div for the header
+        bodyHeadContainer.appendChild(bodyHead);
+
+        // Create the div for the main content
+        const bodyMain = document.createElement('div');
+        bodyMain.classList.add('body-main');
+
+        // Append the header and main content divs to the section
+        bodyContainer.appendChild(bodyHeadContainer);
+        bodyContainer.appendChild(bodyHeadContainerT);
+        bodyContainer.appendChild(bodyMain);
+
+        // Append the section to the document
+        document.body.appendChild(bodyContainer);
+
+        sections.forEach(section => {
+            if (section !== bodyContainer) {
+                section.style.display = 'none';
+            }
+        });
+    };
+
+    for (let card of cards) {
+        card.addEventListener('click', function() {
+            createBodyContainer(this.querySelector('h2').innerText);
+        });
+    }
+
+    for (let link of navLinks) {
+        link.addEventListener('click', function() {
+            createBodyContainer(this.innerText);
+        });
+    }
+})
+.catch(error => {
+    console.error('Error reading JSON file:', error);
+});
